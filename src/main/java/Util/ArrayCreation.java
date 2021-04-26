03db22fb6e18ce444976a31a0f016fa997b005e0
razor <razor@Fight.com>
2021-04-26 21:32:44 +0800

    add CreateRandomArray for test the parallel version of the mergeSort algorithm

--- src/main/java/Util/ArrayCreation.java
+++ src/main/java/Util/ArrayCreation.java
@@ -1,6 +1,17 @@
 package Util;
 
+import java.util.Random;
+
 public class ArrayCreation {
+    public ArrayCreation() {
 
-
+    }
+    private int[] create(int length){
+        Random random = new Random();
+        int[] array = new int[length];
+        for (int i = 0; i <length ; i++) {
+            array[i] = random.nextInt(200);
+        }
+        return array;
+    }
 }
