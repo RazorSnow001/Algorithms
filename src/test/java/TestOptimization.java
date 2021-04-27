639027fff49bdec0f81b17049612f89e4caab822
razor <changjiangmingxj@163.com>
2021-04-28 01:13:13 +0800

    P0084 LargestRectangleInHistogram

--- src/test/java/TestOptimization.java
+++ src/test/java/TestOptimization.java
@@ -3,7 +3,7 @@ import org.testng.annotations.Test;
 
 public class TestOptimization {
     @Test
-    public void test() {
+    public void testLargestRectangle() {
         int[] input = {2, 1, 5, 6, 5, 2, 3, 3, 3, 3, 3, 3};
         LargestRectangleInHistogram search = new LargestRectangleInHistogram();
         search.bruteFindLargest(input);
