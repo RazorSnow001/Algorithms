a2562966d34f97d5851e9e0708af637013cb4ddc
RazorSnow <83212941+RazorSnow001@users.noreply.github.com>
2021-05-01 15:48:44 +0800

    P0004 version1 very bad implementation

---
+++ src/test/java/MedianOfTwoSortedArraysTest.java
@@ -0,0 +1,31 @@
+import Search.MedianOfTwoSortedArrays;
+import Util.ArrayCreation;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+public class MedianOfTwoSortedArraysTest {
+     @Test
+     public  void test(){
+         MedianOfTwoSortedArrays find = new MedianOfTwoSortedArrays();
+         ArrayCreation creation = new ArrayCreation();
+         int[] input = creation.create(10, 20);
+         ArrayCreation creation2 = new ArrayCreation();
+         int[] input2 = creation2.create(5, 20);
+         Arrays.sort(input);
+         Arrays.sort(input2);
+         creation.displayArray(input);
+         System.out.println();
+         creation.displayArray(input2);
+         System.out.println();
+         int[] combine = new int[input.length+input2.length];
+         for (int i = 0; i < combine.length; i++) {
+             combine[i] = (i<input.length)? input[i] : input2[i-input.length];
+         }
+         Arrays.sort(combine);
+         creation.displayArray(combine);
+         System.out.println();
+         System.out.println(find.findMedian(input,input2));
+     }
+
+}
