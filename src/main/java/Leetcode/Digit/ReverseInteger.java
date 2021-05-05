76954d15d0388dc7a43f417ec7b15fe3a02c6123
RazorSnow <changjiangmingxj@163.com>
2021-05-05 22:23:44 +0800

    ReverseInteger

---
+++ src/main/java/Leetcode/Digit/ReverseInteger.java
@@ -0,0 +1,32 @@
+package Leetcode.Digit;
+
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        if (x / 10 == 0) {
+            return x;
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        while (x / 10 != 0) {
+            queue.add(x % 10);
+            x = x / 10;
+        }
+        queue.add(x);
+        int result = 0;
+        while (!queue.isEmpty()) {
+            result += queue.poll() * Math.pow(10, queue.size());
+        }
+        return result;
+    }
+
+    @Test
+    public void test() {
+        ReverseInteger reverseInteger = new ReverseInteger();
+        int result = reverseInteger.reverse(-1230);
+        System.out.println(result);
+    }
+}
