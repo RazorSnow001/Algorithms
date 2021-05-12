84a3bfc072c7ab47115e71992f7f5195e3f8ab8c
RazorSnow <changjiangmingxj@163.com>
2021-05-12 15:18:02 +0800

    P0011 ContainerWithMostWater

---
+++ src/main/java/Leetcode/Optimization/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package Leetcode.Optimization;
+
+/*ok
+ * Area = min(height_left,height_right)*(height_right_index-height_left_index)
+ * 1 use the brute way --- use the two loop to find every left and right index --- compare find the
+ * min height and find the corresponding area and compare
+ * 2 but we see the area function ok  how to use the property to reduce some
+ * redundancy computing
+ * */
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int maxArea = 0, left = 0, right = height.length - 1;
+        while (left < right) {
+            maxArea = Math.max(maxArea, Math.max(height[left], height[right]) * (right - left));
+            if (height[left] < height[right])
+                left++;
+            else
+                right++;
+        }
+        return maxArea;
+    }
+}
