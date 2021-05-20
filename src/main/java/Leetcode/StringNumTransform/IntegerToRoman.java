358fb5979cabd6a812406c12a2ded063567d5bbf
RazorSnow <changjiangmingxj@163.com>
2021-05-20 19:47:59 +0800

    gephi

---
+++ src/main/java/Leetcode/StringNumTransform/IntegerToRoman.java
@@ -0,0 +1,15 @@
+package Leetcode.StringNumTransform;
+
+/* why we need to think the problem in a very complete way ?
+*  we can just write the original  prototype  . yes
+* just 1 - 20
+*  that is simple . and we see some thing that is really similar
+*  17 - 10+5+2 17/10 = 1 17%10=7
+*  and 7/5=1 7%5=2
+*  and 2/1 = 2
+*  17  =  */
+public class IntegerToRoman {
+
+
+
+}
