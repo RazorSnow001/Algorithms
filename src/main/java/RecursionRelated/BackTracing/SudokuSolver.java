d6ef02e532517240eb334e42825bc8a4543f2cae
RazorSnow <changjiangmingxj@163.com>
2021-06-11 21:33:23 +0800

    have fun

--- src/main/java/RecursionRelated/BackTracing/SudokuSolver.java
+++ src/main/java/RecursionRelated/BackTracing/SudokuSolver.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.function.Supplier;
 
 public class SudokuSolver {
     List<Set<Integer>> rowLists = new ArrayList<>();
