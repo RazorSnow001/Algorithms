0419fb3a7cede49885d26f035997e73661658a88
RazorSnow <changjiangmingxj@163.com>
2021-06-11 15:02:40 +0800

    commit

--- src/main/java/RecursionRelated/BackTracing/KnightTour.java
+++ src/main/java/RecursionRelated/BackTracing/KnightTour.java
@@ -1,4 +1,16 @@
 package RecursionRelated.BackTracing;
 
+/*rethink about the problem . that is a back trace problem with just
+* 1 give our a table
+* 2 give the constraint of the knight movement
+* 3 the knight can't return back .
+* 4 we want the knight to traverse all the table .
+* 5 display its first util the last movement .
+* so that very problem can be seen as
+* yi = x1x2...x64   yes the assignment for this value is actually not by the sequence according to
+* the constraint . */
 public class KnightTour {
+
+
+
 }
