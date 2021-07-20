41ca2111b2fbf25b7eb42a7bcc41c671fa17ad12
RazorSnow001 <623274055@qq.com>
2021-07-20 16:51:51 +0800

    ActivitySelectionProblem

---
+++ src/main/java/RecursionRelated/DynamicProgramming/GreedyAlgorithm/Activity.java
@@ -0,0 +1,27 @@
+package RecursionRelated.DynamicProgramming.GreedyAlgorithm;
+
+public class Activity {
+    private String startTime;
+    private String endTime;
+
+    public Activity(String startTime, String endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+}
--- src/main/java/RecursionRelated/DynamicProgramming/GreedyAlgorithm/ActivitySelection.java
+++ src/main/java/RecursionRelated/DynamicProgramming/GreedyAlgorithm/ActivitySelection.java
@@ -1,4 +1,17 @@
 package RecursionRelated.DynamicProgramming.GreedyAlgorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+/*ok 1 we have noticed that --- a greedy algorithm can reduce the branches of the
+*  dynamic programming . we just consider the best choice .so we just remain only
+*  one selection . so the main case of the greedy algorithms is why ? not how ?
+*  we have noticed that if you just look the current best choice .may leads to a
+*  no global best choice .that is the case . but why ? that is the reason we needs to
+*  read the books . ok that is very fascinating ! */
 public class ActivitySelection {
+    private List<Activity> activities = new ArrayList<>();
+
+    public void getTheBestActivities() {
+
+    }
 }
