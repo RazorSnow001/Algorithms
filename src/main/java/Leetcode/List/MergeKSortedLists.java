587f2947407937858c7c451c44eb3d3d9335c5ff
RazorSnow <changjiangmingxj@163.com>
2021-08-02 21:21:29 +0800

    MergeKsortedLists;

--- src/main/java/Leetcode/List/MergeKSortedLists.java
+++ src/main/java/Leetcode/List/MergeKSortedLists.java
@@ -14,9 +14,41 @@ package Leetcode.List;
  * 3 ok that is the way !
  *   and here is another way to think about that !
  *   from the step two . so what is the priority queue in the java ?
+ *   THE priority queue is very great .
+ *   we can put a comparator in the priority queue to tell the detail of what kind of the priority we use !
+ *   so we push all the elements of the list of arrays . into the priority queue
+ *   and every time we pull the queue we get the elements satisfy the rule .
  * */
-public class MergeKSortedLists {
 
+import java.util.*;
+
+public class MergeKSortedLists {
+    List<int[]> input;
+    List<Integer> result = new ArrayList<>();
+
+    public MergeKSortedLists(List<int[]> input) {
+        this.input = input;
+    }
+
+    public void getSortedMergedList() {
+        PriorityQueue<Integer> queue = new PriorityQueue(Comparator.comparingInt(o -> (Integer) o));
+        input.forEach(array -> Arrays.stream(array).forEach(
+                queue::add
+        ));
+        while (!queue.isEmpty()) {
+            result.add(queue.poll());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<int[]> input = Arrays.asList(new int[]{1, 3, 5}, new int[]{7, 11, 16, 22},
+                new int[]{3, 4, 9, 18, 111}, new int[]{1, 2, 7, 9, 10, 33, 56, 67, 89},
+                new int[]{8, 10, 23, 45, 46});
+        MergeKSortedLists merge = new MergeKSortedLists(input);
+        merge.getSortedMergedList();
+        System.out.println(merge.result);
+
+    }
 
 
 }
--- src/main/java/RecursionRelated/BackTracing/KnightTour.java
+++ src/main/java/RecursionRelated/BackTracing/KnightTour.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-/*1 what is the constraint of the problem ?
+/*
+ *   1 what is the constraint of the problem ?
  *   ok the constraint is the
  *       1  the movement of the knight
  *       2  the boundary condition which also limit the knight movements
