194c15399deab178a1420b8b4f75464df15ca534
RazorSnow <changjiangmingxj@163.com>
2021-08-16 03:35:57 +0800

    String Sorting KeyIndexCounting

--- src/main/java/String/KeyIndexedCounting/KeyIndexedCounting.java
+++ src/main/java/String/KeyIndexedCounting/KeyIndexedCounting.java
@@ -1,5 +1,7 @@
 package String.KeyIndexedCounting;
 
+import java.util.*;
+
 
 /*
 !that is not very complicated problem
@@ -17,10 +19,53 @@ package String.KeyIndexedCounting;
 !
 !  3 use the brute no encapsulated algorithm
 !    we use the array to do the record
+
+     ok first
+    1 we need an array to record the number of each group of the elements
+    2 we need to add some items by the order to get the first index of the first element of each group
+    3 we need to use the executed record array to execute the actual index of each item
+
+    the implementation of this record array is pretty impressive my bro
+    it actually records the index of the eventual result .
+    but it uses the relationship between the first index of each group and the group numbers and orders
+    so that is a very elegant solution .
 */
 public class KeyIndexedCounting {
+    public List<Person> sortByGroupId(List<Person> personList) {
+        Optional<Integer> result = personList.stream().map(Person::getGroup).max(Comparator.comparingInt(a -> a));
+        Comparator<Person> personComparator = Comparator.comparingInt(Person::getGroup);
+        PriorityQueue<Person> priorityQueue = new PriorityQueue(personComparator);
+        List<Person> sortedPersonList = new ArrayList<>();
+        if (result.isPresent()) {
+            int[] record = new int[result.get() + 2];
+            for (Person person : personList) {
+                record[person.getGroup() + 1]++;
+            }
+            for (int i = 0; i < record.length - 1; i++) {
+                record[i + 1] = record[i] + record[i + 1];
+            }
+            for (Person person : personList) {
+                record[person.getGroup()]++;
+                int resultIndex = record[person.getGroup()];
+                person.setIndex(resultIndex);
+                priorityQueue.offer(person);
+            }
+            while(!priorityQueue.isEmpty()){
+                sortedPersonList.add(priorityQueue.poll());
+            }
+        }
+        return sortedPersonList;
+    }
 
 
+    public static void main(String[] args) {
+        List<Person> personList = Arrays.asList(new Person("TOM", 2), new Person("LUCY", 2),
+                new Person("JOSH", 1), new Person("BOSH", 3));
+        KeyIndexedCounting sortByGroupId = new KeyIndexedCounting();
+        List<Person> sortedPersonList = sortByGroupId.sortByGroupId(personList);
+        sortedPersonList.forEach(p -> System.out.println(p.getName() + "..." + p.getGroup()));
+
+    }
 
 
 }
---
+++ src/main/java/String/KeyIndexedCounting/Person.java
@@ -0,0 +1,21 @@
+package String.KeyIndexedCounting;
+
+import lombok.Data;
+
+@Data
+public class Person {
+    private String name;
+    private Integer group;
+    private Integer index;
+
+    public Person(String name, Integer group, Integer index) {
+        this.name = name;
+        this.group = group;
+        this.index = index;
+    }
+
+    public Person(String name, Integer group) {
+        this.name = name;
+        this.group = group;
+    }
+}
