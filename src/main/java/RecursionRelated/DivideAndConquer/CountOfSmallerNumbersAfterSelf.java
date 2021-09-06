488f1259c2fb141c5f0599dcd647518ea3647eeb
Razor <changjiangmingxj@163.com>
2021-09-06 16:22:12 +0800

    merge sort to get the reversed num of the sequence !

--- src/main/java/RecursionRelated/DivideAndConquer/CountOfSmallerNumbersAfterSelf.java
+++ src/main/java/RecursionRelated/DivideAndConquer/CountOfSmallerNumbersAfterSelf.java
@@ -1,9 +1,19 @@
 package RecursionRelated.DivideAndConquer;
 
 
-/* *leetcode 0315 Count of Smaller Numbers After Self
+/* *    Leetcode 0315 Count of Smaller Numbers After Self
    *    The reverse of  the number array .
    *    that is very great problem .
+   *    so this problem can be seen as the reuse of the reversedNum problem
+   *    for example
+   *    7 5 3 1
+   *    3 2 1 0  so that is
+   *    so for the sub problem we have 5 3 1 and we get the 2  but we actually use the merge sort to do that
+   *    so 5 3 1 we get the 2 and 1 3 5 ok
+   *    1 3 7 5
+   *    0 0 1 0
+   *
+   *
    *
 * */
 public class CountOfSmallerNumbersAfterSelf {
