c184af9e92ff6e6fbf7e6671fbe134d9178d81ec
Razor <changjiangmingxj@163.com>
2022-02-09 14:53:15 +0800

    2022-02-09-commit_2

---
+++ src/main/java/JavaFundamentals/Concurrency/ExecutorTest.java
@@ -0,0 +1,19 @@
+package JavaFundamentals.Concurrency;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorTest {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
+
+
+
+
+    }
+
+
+
+}
